8d04e04301961b0c907a044b44fd560028bec5c4
awat <154649552+Away12568@users.noreply.github.com>
2024-08-02 11:21:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,107 @@
+// function hello(a, b, c) { 
+//     console.log(a + b + c);
+// }
+
+// hello(10, 20, 30)
+
+// const fn = function(a, b) {
+//     console.log(a + b);
+// }
+
+
+// fn
+
+
+
+// const plus = (a, b) => a + b
+
+
+
+
+
+// console.log(plus(30, 3));
+
+
+
+
+// function plus(a, b) {
+//     console.log(a + b);
+// }
+
+// plus(10, 20)
+
+
+
+
+// const minus = function ( a, b) {
+//     console.log(a - b);
+// }
+
+// minus(20, 10)
+
+
+
+// const divide = (a, b) => { 
+//     console.log(a / b);
+// }
+
+// divide( 25, 5)
+
+
+
+// const multiplication = (a, b) => a * b 
+
+// console.log(multiplication(5,5));
+
+// const me = (a, b, c) => {
+//     console.log( "привет меня зовут" + a + " мне " + c + " лет");
+    
+// }
+
+// me(" чингиз", " галиев",  15  )
+
+
+
+
+
+
+
+
+
+
+// const fn2  = (a) => {
+// if ( a < 0) 
+//     { console.log("это отрицательное число");
+//     }
+
+// else if (a == 0 ) { console.log("это ноль");
+// }
+
+
+
+//  else if ( a == 2){
+//     console.log("это второй!");
+//  }
+
+//  else if ( a > 0) {console.log("это положительное число");
+//  }
+//  else { console.log( "это не второй");
+//  }
+// }
+
+
+//  fn2(  prompt("введите число"))
+
+
+
+const persone = ( name, surname, age ) => {
+ if ( age >= 18 ) { console.log(`привет ${name} ${surname} мне ${age}`);}
+   
+ else { console.log( "иди домой");
+}
+}
+
+
+persone( "awat", "awat", 1)
+
+
